b3f6e3715911bc240d50def146845adae349ed65
ryoma.takahashigit config --global user.email takahashiryouma0102@gmail.com <ubuntu@ip-172-31-30-8.ap-northeast-1.compute.internal>
2021-11-10 08:20:07 +0000

    team.rb

---
+++ team.rb
@@ -0,0 +1,32 @@
+class Team
+  attr_accessor :name, :win, :lose, :draw
+
+def initialize(name,win,lose,draw) 
+  self.name = name
+  self.win = win
+  self.lose = lose 
+  self.draw = draw
+end
+
+def calc_win_rate()
+  return self.win.to_f / (self.win.to_f + self.lose.to_f)
+end
+
+def show_team_result()
+  puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate()}です。"
+end   
+end
+
+
+giants = Team.new('Giants', 67, 45, 8)
+tigers = Team.new('Tigers', 60, 53, 7)
+dragons = Team.new('Dragons', 60, 55, 7)
+baystars = Team.new('BayStars', 56, 58, 6)
+carp = Team.new('Carp', 52, 56, 12)
+swallows = Team.new('Swallows', 41, 69, 10)
+
+teams = [giants, tigers, dragons, baystars, carp, swallows] 
+teams.each do |team|
+team.show_team_result()
+end
+ 
